fd8a59cd011b2d5ded69cfa39eb2655a910e5e33
Tamar Auber <mischegoss@gmail.com>
2018-10-18 11:52:30 -0400

    feat: divided into components using DB method

--- src/component/BookCase.js
+++ src/component/BookCase.js
@@ -1,1 +1,71 @@
-//This is my bookcase component
+import React, {Component} from 'react'
+// import * as BooksAPI from './BooksAPI'
+import BookShelf from "./BookShelf";
+
+class BookCase extends Component {
+    state = {}
+
+    componentDidMount = () => {
+        // Update the list of all books
+        this
+            .props
+            .onRefreshAllBooks();
+    }
+
+    updateShelves = () => {
+        // Update the state of the individual shelves to contain the appropriate books
+        // for each
+        const newCurrent = {
+            name: "Currently Reading",
+            books: this
+                .props
+                .books
+                .filter(book => book.shelf === 'currentlyReading')
+        };
+        const newWant = {
+            name: "Want to Read",
+            books: this
+                .props
+                .books
+                .filter(book => book.shelf === "wantToRead")
+        };
+        const newRead = {
+            name: "Read",
+            books: this
+                .props
+                .books
+                .filter(book => book.shelf === "read")
+        };
+
+        return ([newCurrent, newWant, newRead]);
+    }
+
+    render() {
+        let shelves = [];
+        if (this.props.books && this.props.books.length)
+            shelves = this.updateShelves();
+
+        return (
+            <div className="app">
+                <div className="list-books">
+                    <div className="list-books-title">
+                        <h1>MyReads</h1>
+                    </div>
+                    <div className="list-books-content">
+                        <div>
+                            {shelves && shelves.map((shelf) => (<BookShelf
+                                key={shelf.name}
+                                shelf={shelf}
+                            />))}
+                        </div>
+                    </div>
+                    <div className="open-search">
+                        <a onClick={() => this.setState({showSearchPage: true})}>Add a book</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BookCase;
